69d2ff16bce5d8601cf5b682448ac107560fe54c
yambaolr <yambaolorinrobelle@gmail.com>
2025-11-20 07:56:56 +0800

    ✨ added new components

--- Prelim Lab Activities/Activity 2/app.ts
+++ Prelim Lab Activities/Activity 2/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { Proficiencies } from "./proficiencies/proficiencies";
+import { References } from "./references/references";
 import { Skills } from "./skills/skills";
 import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Skills, Footer],
+  imports: [RouterOutlet, Header, Proficiencies, References, Skills, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
